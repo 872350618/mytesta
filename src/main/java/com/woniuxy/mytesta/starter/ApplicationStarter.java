191db864e04e699d4a30adbe9b2592f51a8c8c47
a <a@163.com>
2020-05-28 14:38:14 +0800

    add bbb

--- src/main/java/com/woniuxy/mytesta/starter/ApplicationStarter.java
+++ src/main/java/com/woniuxy/mytesta/starter/ApplicationStarter.java
@@ -3,5 +3,6 @@ package com.woniuxy.mytesta.starter;
 public class ApplicationStarter {
     public static void main(String[] args) {
         System.out.println("aaa");
+        System.out.println("bbb");
     }
 }
