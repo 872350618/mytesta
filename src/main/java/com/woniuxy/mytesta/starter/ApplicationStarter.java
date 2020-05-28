f8231ffe2c75f69a64e8f762ed3c332de7367e18
a <a@163.com>
2020-05-28 14:44:04 +0800

    add ccc

--- src/main/java/com/woniuxy/mytesta/starter/ApplicationStarter.java
+++ src/main/java/com/woniuxy/mytesta/starter/ApplicationStarter.java
@@ -4,5 +4,6 @@ public class ApplicationStarter {
     public static void main(String[] args) {
         System.out.println("aaa");
         System.out.println("bbb");
+        System.out.println("ccc");
     }
 }
